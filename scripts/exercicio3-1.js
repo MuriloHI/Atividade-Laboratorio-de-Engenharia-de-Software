20df1d96f63667aa95f3d81f45a54940ab9e24a9
MuriloHI <murilo.hidekiichioka@gmail.com>
2025-02-26 12:20:56 -0300

    Começo das atividades da semana 3

---
+++ scripts/exercicio3-1.js
@@ -0,0 +1,7 @@
+document.getElementById("meuBt").addEventListener("click", exibirOrdemAlfaEmListaOrde);
+
+function exibirOrdemAlfaEmListaOrde() {
+    document.write
+}
+
+/* sort(): ordena o array; usa esse pra colocar em ordem alfabética */
---
+++ scripts/exercicio3-2.js
@@ -0,0 +1,6 @@
+function Carro() {
+    let marca;
+    let ano
+
+
+}
