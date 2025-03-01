3c82877668596983bdd3c01d300208caace190e9
MuriloHI <murilo.hidekiichioka@gmail.com>
2025-03-01 18:45:37 -0300

    Terminando os exercícios 1 e 2 da Semana 3, que foram começados em sala de aula

--- scripts/exercicio3-1.js
+++ scripts/exercicio3-1.js
@@ -1,7 +1,35 @@
+let valores = []
+
 document.getElementById("meuBt").addEventListener("click", exibirOrdemAlfaEmListaOrde);
 
 function exibirOrdemAlfaEmListaOrde() {
-    document.write
+    let input = document.querySelector("input");
+    let nome = input.value;
+
+    if (nome != "")
+    {
+        valores.push(nome);
+        valores.sort();
+        atualizarLista();
+    }
+    input.value = ""; /* limpa o campo do input, se eu tirar e digitar "Nome 1" e clicar em adicionar, o "Nome 1" ainda vai estar la digitado*/
 }
 
-/* sort(): ordena o array; usa esse pra colocar em ordem alfabética */
+function atualizarLista(){
+    let listaOrdenada = document.getElementById("listaOrdenada");
+    listaOrdenada.innerHTML = ""; 
+
+
+    valores.forEach
+    (
+        function(valor)
+        { 
+            let item = document.createElement("li"); /*criando um li na lista ordenada, um item, mas ainda nao criei o TEXTO dentro do li*/
+            let texto = document.createTextNode(valor); /* criando um nó de texto, ele ainda n esta em <li> */
+
+            item.appendChild(texto); /* agr sim, o let texto (algum nome) está no let item (que é um <li>) */
+            
+            listaOrdenada.appendChild(item); /* agr falta adicionar o <li> dentro do <ol>, que temos o id como #id="listaOrdenada" */
+        }
+    );
+}
--- scripts/exercicio3-2.js
+++ scripts/exercicio3-2.js
@@ -1,6 +1,44 @@
-function Carro() {
-    let marca;
-    let ano
+function Carro(marca, modelo, ano, kilometragem, valor_fipe) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.ano = ano;
+    this.kilometragem = kilometragem;
+    this.valor_fipe = valor_fipe;
 
+    /* método: calcular quantos anos de utilização o carro tem */
+    this.anosUtilizacao = function()
+    {
+        const anoAtual = new Date().getFullYear();
+        return anoAtual - this.ano;
+    }
 
+    /* método: calcular o valor de mercado do carro (com as regras do exercício) */
+    this.valorMercado = function()
+    {
+        let kmPorAno = this.kilometragem/this.anosUtilizacao();
+        if (kmPorAno <= 30000){
+            return this.valor_fipe * 1.1;
+        }
+        else if (kmPorAno <= 50000){
+            return this.valor_fipe;
+        }
+        else {
+            return this.valor_fipe * 0.9;
+        }
+    }
 }
+
+
+document.getElementById("meuBt").addEventListener("click", function() {
+    /* pegando os inputs lá do html */
+    let marca = document.getElementById("marca").value;
+    let modelo = document.getElementById("modelo").value;
+    let ano = parseInt(document.getElementById("ano").value);
+    let kilometragem = parseInt(document.getElementById("kilometragem").value);
+    let valor_fipe = parseFloat(document.getElementById("valor_fipe").value);
+
+    /* criando o objeto carro da classe Carro */
+    let carro = new Carro(marca, modelo, ano, kilometragem, valor_fipe);
+
+    alert(`O carro tem ${carro.anosUtilizacao()} anos de utilização e o valor de mercado é R$ ${carro.valorMercado().toFixed(2)}.`);
+});
